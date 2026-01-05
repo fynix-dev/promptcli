c125a643458350ed70f9407125df586b7239f6a5
Aadi Narayanan R <154123805+fynix-dev@users.noreply.github.com>
2026-01-05 22:36:29 +0530

    Uncomment NavBar component in layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${spaceMono.variable} ${inter.variable} bg-primary-bg antialiased`}
       >
-        {/* <NavBar /> */}
+        <NavBar />
         {children}
       </body>
     </html>
