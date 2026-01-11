6f28bbd1f40769e43b19b44ac1160967acfc4628
Aadi Narayanan R <aadinarayanan330@gmail.com>
2026-01-11 19:36:35 +0530

    Initial working version of create route deployed to vercel

--- components/Input.tsx
+++ components/Input.tsx
@@ -34,7 +34,7 @@ const Input = ({setGeneratedPrompt, setChat, setQuestions, setLoadingPrompt}: In
 
       setLoadingPrompt(true);
       try {
-        const response = await fetch('https://fynix-dev.github.io/promptcli/api/ai', {
+        const response = await fetch('https://promptcli.vercel.app/api/ai', {
           method: "POST",
           headers: {
               "Content-Type": "application/json"
