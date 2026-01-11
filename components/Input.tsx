3a452a83232d3544a82aab238f3808f941182f69
Aadi Narayanan R <aadinarayanan330@gmail.com>
2026-01-11 21:43:42 +0530

    Changed the output export setting in next config

--- components/Input.tsx
+++ components/Input.tsx
@@ -34,7 +34,7 @@ const Input = ({setGeneratedPrompt, setChat, setQuestions, setLoadingPrompt}: In
 
       setLoadingPrompt(true);
       try {
-        const response = await fetch('https://promptcli.vercel.app/api/ai', {
+        const response = await fetch('/api/ai', {
           method: "POST",
           headers: {
               "Content-Type": "application/json"
