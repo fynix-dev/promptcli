88fea7b0631ac479051880672b112edbb40f4b8f
Aadi Narayanan R <aadinarayanan330@gmail.com>
2026-01-11 19:27:09 +0530

    Initial working version of create route

--- components/Input.tsx
+++ components/Input.tsx
@@ -34,7 +34,7 @@ const Input = ({setGeneratedPrompt, setChat, setQuestions, setLoadingPrompt}: In
 
       setLoadingPrompt(true);
       try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/ai`, {
+        const response = await fetch('https://fynix-dev.github.io/promptcli/api/ai', {
           method: "POST",
           headers: {
               "Content-Type": "application/json"
